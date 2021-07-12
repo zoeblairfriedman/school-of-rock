e2f633d0008e5b5c0e0f4297043e4184da77e1f8
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-07-12 14:36:46 -0400

    migrates from Sqlite3 to Postgres

--- school-of-rock-backend/db/schema.rb
+++ school-of-rock-backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_04_141854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rocks", force: :cascade do |t|
     t.string "name"
     t.string "body"
